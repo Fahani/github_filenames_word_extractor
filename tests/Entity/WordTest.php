c9ed7e8ceb1b770ab482d03df7166085f2bb59d1
Nico <fahani45@gmail.com>
2018-09-25 18:26:41 +0200

    Doing TDD, testing name and frequency will persist in class Word. Test failed, time to code

---
+++ tests/Entity/WordTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niko
+ * Date: 25/09/2018
+ * Time: 18:16
+ */
+
+namespace App\Tests\Entity;
+
+
+use PHPUnit\Framework\TestCase;
+
+class WordTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function checkNameAndFrequencyPersist()
+    {
+        $word = new Word("Main", 10);
+
+        $this->assertSame(10, $word->getFrequency());
+        $this->assertSame("Main", $word->getName());
+    }
+}
