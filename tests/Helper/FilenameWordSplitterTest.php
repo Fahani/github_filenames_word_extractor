679bfb1f0008bca144fc36512523804470821588
Nico <fahani45@gmail.com>
2018-09-25 20:33:54 +0200

    TDD. Testing a Helper that will split the filename into words. Test failed

---
+++ tests/Helper/FilenameWordSplitterTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niko
+ * Date: 25/09/2018
+ * Time: 20:32
+ */
+
+namespace App\Tests\Helper;
+
+
+use PHPUnit\Framework\TestCase;
+
+class FilenameWordSplitterTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function filenameIsSplitIntoWords()
+    {
+        $words = FilenameWordSplitter::filenameToWords("", "php");
+
+        $this->assertEmpty($words);
+
+        $words = FilenameWordSplitter::filenameToWords("another_folder/FileSix.php", "php");
+
+        $this->assertCount(2, $words);
+        $this->assertSame("File", $words[1]);
+        $this->assertSame("Six", $words[2]);
+
+        $words = FilenameWordSplitter::filenameToWords("another_folder/FileSix.php", ".go");
+
+        $this->assertEmpty($words);
+    }
+}
