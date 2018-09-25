629c924095b91baab12602aba1fdb7d8ae3ae529
Nico <fahani45@gmail.com>
2018-09-25 18:55:40 +0200

    TDD. Created a test class for a new service, WordCounter. This service will hold an array of the Words used in the repository and its frequency. Test failed

---
+++ tests/Service/WordCounterTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niko
+ * Date: 25/09/2018
+ * Time: 18:47
+ */
+
+namespace App\Tests\Service;
+
+
+use PHPUnit\Framework\TestCase;
+
+class WordCounterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function getAndSetWordsWorksAsExpected()
+    {
+        $wordCounter = new WordCounter();
+
+        $this->assertEmpty($wordCounter->getWords());
+
+        $wordCounter->addWord("");
+
+        $this->assertEmpty($wordCounter->getWords());
+
+        $wordCounter->addWord("File");
+
+        $this->assertCount(1, $wordCounter->getWords());
+        $this->assertSame(1, $wordCounter->getWords()["File"]->getFrenquency());
+        $this->assertSame("File", $wordCounter->getWords()["File"]->getName());
+
+        $wordCounter->addWord("File");
+
+        $this->assertCount(1, $wordCounter->getWords());
+        $this->assertSame(2, $wordCounter->getWords()["File"]->getFrequency());
+        $this->assertSame("File", $wordCounter->getWords()["File"]->getName());
+
+        $wordCounter->addWord("Six");
+
+        $this->assertCount(2, $wordCounter->getWords());
+        $this->assertSame(2, $wordCounter->getWords()["File"]->getFrequency());
+        $this->assertSame("File", $wordCounter->getWords()["File"]->getName());
+        $this->assertSame(1, $wordCounter->getWords()["Six"]->getFrenquency());
+        $this->assertSame("Six", $wordCounter->getWords()["Six"]->getName());
+    }
+}
