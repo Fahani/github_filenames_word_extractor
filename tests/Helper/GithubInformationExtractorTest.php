3ab7e185c49c1db266600ec48e6a847496b9aafc
Nico <fahani45@gmail.com>
2018-09-25 20:39:15 +0200

    TDD. Created a new test for the Helper GithubInformationExtractor. I test we can get an array with the filenames given a an array with the commit information. Test failed

---
+++ tests/Helper/GithubInformationExtractorTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niko
+ * Date: 25/09/2018
+ * Time: 20:37
+ */
+
+namespace App\Tests\Helper;
+
+
+use PHPUnit\Framework\TestCase;
+
+class GithubInformationExtractorTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function getFileNamesFromGithubCommitInformation()
+    {
+        $commitInformation = [];
+
+        $fileNames = GithubInformationExtractor::getFileNameInCommitInformation($commitInformation);
+
+        $this->assertEmpty($fileNames);
+
+        $commitInformation = [
+            "files" => []
+        ];
+
+        $fileNames = GithubInformationExtractor::getFileNameInCommitInformation($commitInformation);
+
+        $this->assertEmpty($fileNames);
+
+        $commitInformation = [
+            "files" => [
+                ["filename" => "another_folder/FileSix.php"]
+            ]
+        ];
+
+        $fileNames = GithubInformationExtractor::getFileNameInCommitInformation($commitInformation);
+
+        $this->assertCount(1, $fileNames);
+        $this->assertSame("another_folder/FileSix.php", $fileNames[0]);
+
+        $commitInformation = [
+            "files" => [
+                ["filename" => "another_folder/FileSix.php"],
+                ["filename" => "another_folder/FileFive.php"]
+            ]
+        ];
+
+        $fileNames = GithubInformationExtractor::getFileNameInCommitInformation($commitInformation);
+
+        $this->assertCount(2, $fileNames);
+        $this->assertSame("another_folder/FileSix.php", $fileNames[0]);
+        $this->assertSame("another_folder/FileFive.php", $fileNames[1]);
+    }
+
+}
