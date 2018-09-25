6526d73c2ff12cbe0b0a3412be50636425564954
Nico <fahani45@gmail.com>
2018-09-25 20:07:45 +0200

    TDD. Testing the we can get all the commits from a repository. Test fails

--- tests/Service/GithubClientTest.php
+++ tests/Service/GithubClientTest.php
@@ -33,15 +33,36 @@ class GithubClientTest extends TestCase
      * @test
      * @throws \Exception
      */
-    public function commitInformationThrowsExceptionOnEmptySHA()
+    public function returnsAllTheCommitsInRepository()
     {
-        $clientMock = $this->prophesize(Client::class);
-        $resultPagerMock = $this->prophesize(ResultPager::class);
+        $clientMock             = $this->prophesize(Client::class);
+        $commitsApiClient       = $this->prophesize(Commits::class);
+        $repositoriesApiClient  = $this->prophesize(Repo::class);
+        $resultPagerMock        = $this->prophesize(ResultPager::class);
 
-        $this->expectException("Exception");
+        $clientMock->authenticate(getenv('GITHUB_SECRET'), null, getenv('GITHUB_AUTH_METHOD'))
+            ->shouldBeCalledTimes(1);
+
+        $clientMock->repo()
+            ->shouldBeCalledTimes(1)
+            ->willReturn($repositoriesApiClient);
+
+        $repositoriesApiClient->commits()
+            ->willReturn($commitsApiClient);
+
+        $resultPagerMock->fetchAll($commitsApiClient, "all", array("fahani", "colvin", ['sha' => 'master']))
+            ->shouldBeCalledTimes(1)
+            ->willReturn([
+                ["sha" => "90ec51b93624438947df6704ecb8982d38454ef4"],
+                ["sha" => "78a3b378fc5d7d2d4aebf6f0487bc0cbaf9c63ff"],
+            ]);
 
         $githubClient = new GithubClient($clientMock->reveal(), $resultPagerMock->reveal());
 
-        $githubClient->getCommitInformation("user", "repo", "");
+        $commits = $githubClient->getAllCommitsInRepository("fahani", "colvin");
+
+        $this->assertCount(2, $commits);
+        $this->assertSame("90ec51b93624438947df6704ecb8982d38454ef4", $commits[0]["sha"]);
+        $this->assertSame("78a3b378fc5d7d2d4aebf6f0487bc0cbaf9c63ff", $commits[1]["sha"]);
     }
 }
